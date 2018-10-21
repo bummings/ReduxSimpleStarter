4131878698811f86ab0d91b8546641449ed2517a
bummings <eabummings@gmail.com>
2018-10-21 14:28:22 -0400

    added BookReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,17 @@
+// function() {
+//   return [
+//     { title: 'Brothers Karamazov' },
+//     { title: 'Down and Out Paris and London ' },
+//     { title: 'Of Human Bondage' },
+//     { title: 'The Original of  Laura'  }
+//   ]
+// }
+
+export default () => {
+  return [
+    { title: "Brothers Karamazov" },
+    { title: "Down and Out Paris and London " },
+    { title: "Of Human Bondage" },
+    { title: "The Original of  Laura" }
+  ];
+};
